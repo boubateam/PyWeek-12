6d26878dfa87e038cb06f05558fdc75b8a00fd49
Grégoire Paris <github.token>
2011-04-06 00:23:47 +0200

    add prelevel scene before each level

---
+++ gamelib/prelevel.py
@@ -0,0 +1,36 @@
+'''Scene played before a level to get the player ready
+'''
+
+import pygame
+import scene
+import data
+
+class PreLevelScene(scene.Scene):
+    '''Represents an abstract scene.
+    '''
+
+    name = ''
+
+    def __init__(self, game, name, index, config=None):
+        super(PreLevelScene, self).__init__(game, name, index, config)
+
+        self.text     = data.render_text('genotype.ttf', 30, 'Get ready', (255, 0, 0))
+        self.textrect = self.text.get_rect()
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+    def handleEvent(self, event):
+        pass 
+
+    def update(self):
+        self.textrect.center = (320, 240)
+
+    def draw(self, screen):
+        screen.fill((200, 200, 200))
+        screen.blit(self.text, self.textrect)
+        pygame.time.delay(3000)
+        self.game.director.endScene()
