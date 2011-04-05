7dc5c71ad85f6ff5345f5e2e3b70c963de0db978
Grégoire Paris <github.token>
2011-04-05 22:54:48 +0200

    use genotype in the game over scene

--- gamelib/gameover.py
+++ gamelib/gameover.py
@@ -9,7 +9,7 @@ class GameOverScene(scene.Scene):
     def __init__(self, game, name, index, config=None):
         super(GameOverScene, self).__init__(game, name, index, config)
 
-        self.text = data.render_text(None, 37, 'Game Over', (255, 255, 255))
+        self.text = data.render_text('genotype.ttf', 37, 'Game Over', (255, 255, 255))
         self.textrect = self.text.get_rect()
         self.background = data.load_image('gameover.png')
 
